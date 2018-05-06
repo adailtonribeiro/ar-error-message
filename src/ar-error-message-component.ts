0665722284943ce419c1f421af821ecb7cf811cb
adailtonribeiro <adailton.ribeiro1@gmail.com>
2018-05-06 11:24:08 -0300

    feat(code): add prettier code formatter

--- src/ar-error-message-component.ts
+++ src/ar-error-message-component.ts
@@ -1,6 +1,6 @@
 import {Component, ElementRef, Input, OnDestroy, OnInit} from '@angular/core';
-import {trigger, state, transition, style, animate} from "@angular/animations";
-import {Events} from "ionic-angular";
+import {trigger, state, transition, style, animate} from '@angular/animations';
+import {Events} from 'ionic-angular';
 
 const HTML_TEMPLATE = `<div [@fadeIn] [@fadeOut]="fadeOutEffect" class="ar-error-message">
   <p [ngStyle]="{'display':fadeOutEffect == 'hidden' ? 'none' : 'block' }">{{message}}</p>
@@ -20,68 +20,65 @@ const CSS_STYLE = `
 }`;
 
 @Component({
-    selector: 'ar-error-message',
-    template: HTML_TEMPLATE,
-    styles: [CSS_STYLE],
-    animations: [
-        trigger('fadeIn', [
-            state('void', style({opacity: '0'})),
-            state('*', style({opacity: '1'})),
-            transition('void <=> *', animate('750ms ease-in'))
-        ]),
-        trigger('fadeOut', [
-            //state('*', style({'min-height': '40px'})),
-            state('hidden', style({'min-height': '0'})),
-            transition('* => *', animate('500ms ease-in'))
-        ])
-    ]
+  selector: 'ar-error-message',
+  template: HTML_TEMPLATE,
+  styles: [CSS_STYLE],
+  animations: [
+    trigger('fadeIn', [
+      state('void', style({opacity: '0'})),
+      state('*', style({opacity: '1'})),
+      transition('void <=> *', animate('750ms ease-in'))
+    ]),
+    trigger('fadeOut', [
+      //state('*', style({'min-height': '40px'})),
+      state('hidden', style({'min-height': '0'})),
+      transition('* => *', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class ArErrorMessageComponent implements OnInit, OnDestroy {
+  autoHide: boolean = false;
+  fadeOutEffect: string;
+  @Input('autoScroll') autoScroll: boolean = true;
+  @Input('message') message: string = '';
 
-    autoHide: boolean = false;
-    fadeOutEffect: string;
-    @Input('autoScroll') autoScroll: boolean = true;
-    @Input('message') message: string = '';
-
-    @Input('autoHide') set setAutoHide(autoHide) {
-        this.autoHide = autoHide;
-        if (autoHide) {
-            this.triggerHideComponent();
-        }
-    };
-
-    constructor(public elRef: ElementRef, public events: Events) {
+  @Input('autoHide')
+  set setAutoHide(autoHide) {
+    this.autoHide = autoHide;
+    if (autoHide) {
+      this.triggerHideComponent();
     }
+  }
 
-    ngOnInit() {
-        if (this.autoScroll) {
-            this.scrollToComponent();
-        }
-        this.events.subscribe('form-submit-failed', () => {
-            if (this.autoScroll) {
-                this.scrollToComponent();
-            }
-            if (this.autoHide) {
-                this.fadeOutEffect = 'void';
-                this.triggerHideComponent();
-            }
-        });
+  constructor(public elRef: ElementRef, public events: Events) {}
 
+  ngOnInit() {
+    if (this.autoScroll) {
+      this.scrollToComponent();
     }
+    this.events.subscribe('form-submit-failed', () => {
+      if (this.autoScroll) {
+        this.scrollToComponent();
+      }
+      if (this.autoHide) {
+        this.fadeOutEffect = 'void';
+        this.triggerHideComponent();
+      }
+    });
+  }
 
-    ngOnDestroy() {
-        this.events.unsubscribe('form-submit-failed');
-    }
+  ngOnDestroy() {
+    this.events.unsubscribe('form-submit-failed');
+  }
 
-    triggerHideComponent() {
-        setTimeout(() => {
-            this.fadeOutEffect = 'hidden';
-            //this.message = '';
-        }, 5000);
-    }
+  triggerHideComponent() {
+    setTimeout(() => {
+      this.fadeOutEffect = 'hidden';
+      //this.message = '';
+    }, 5000);
+  }
 
-    scrollToComponent() {
-        this.elRef.nativeElement.scrollIntoView({behavior: "smooth"});
-    }
-
+  scrollToComponent() {
+    this.elRef.nativeElement.scrollIntoView({behavior: 'smooth'});
+  }
 }
--- src/ar-error-message.module.ts
+++ src/ar-error-message.module.ts
@@ -1,24 +1,22 @@
 import {NgModule, ModuleWithProviders} from '@angular/core';
-import {ArErrorMessageComponent} from "./ar-error-message-component";
-import {IonicModule} from "ionic-angular";
+import {ArErrorMessageComponent} from './ar-error-message-component';
+import {IonicModule} from 'ionic-angular';
 
 @NgModule({
-    imports: [
-        IonicModule
-    ],
-    declarations: [
-        // declare all components that your module uses
-        ArErrorMessageComponent
-    ],
-    exports: [
-        // export the component(s) that you want others to be able to use
-        ArErrorMessageComponent
-    ]
+  imports: [IonicModule],
+  declarations: [
+    // declare all components that your module uses
+    ArErrorMessageComponent
+  ],
+  exports: [
+    // export the component(s) that you want others to be able to use
+    ArErrorMessageComponent
+  ]
 })
 export class ArErrorMessageModule {
-    static forRoot(): ModuleWithProviders {
-        return {
-            ngModule: ArErrorMessageModule
-        };
-    }
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: ArErrorMessageModule
+    };
+  }
 }

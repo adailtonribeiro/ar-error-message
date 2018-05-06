0dfedfec1efafe33c452f6793e32994c9c167eb4
adailtonribeiro <adailton.ribeiro1@gmail.com>
2018-05-06 14:19:05 -0300

    chore(commitlint): change from config-angular to config-conventional

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    extends: ['@commitlint/config-angular'],
-    //rules: {}
+  //extends: ['@commitlint/config-angular'],
+  // NOTE: It's used config-conventional because config-angular doesn't allow 'chore' any longer
+  extends: ['@commitlint/config-conventional']
+  //rules: {}
 };
